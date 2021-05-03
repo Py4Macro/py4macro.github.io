49c90fec7fa101c299ec1f4721e631584dbd6688
Tetsu Haruyama <tetsu.yes@gmail.com>
2021-05-03 10:52:54 +0900

    1_Python_Basics

--- _build/jupyter_execute/index.py
+++ _build/jupyter_execute/index.py
@@ -1,22 +1,40 @@
-# Pythonで学ぶ中級マクロ経済学
+#!/usr/bin/env python
+# coding: utf-8
 
-```{epigraph}
-[春山 鉄源](https://t-haruyama.github.io)
+# # Pythonで学ぶ中級マクロ経済学
+# 
+# ```{epigraph}
+# [春山 鉄源](https://t-haruyama.github.io)
+# 
+# 神戸大学経済学研究科
+# ```
 
-神戸大学経済学研究科
-```
+# In[1]:
+
 
 import datetime
 dt = datetime.datetime.now()
 print('Version:',dt.strftime('%Y年%m月%d日'))
 
-<!---
-%H:%M:%S
-dt = datetime.datetime.now()
-dt = datetime.datetime.today()
--->
 
-
-近日公開予定
-
-[「Pythonで学ぶ入門計量経済学」はここをクリック](https://py4etrics.github.io)
+# <!---
+# %H:%M:%S
+# dt = datetime.datetime.now()
+# dt = datetime.datetime.today()
+# -->
+# 
+# 
+# 近日公開予定
+# 
+# [「Pythonで学ぶ入門計量経済学」はここをクリック](https://py4etrics.github.io)
+# 
+# 
+# 
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# :caption: １. Pythonの学習
+# 
+# 1_Python_Basics
+# ```
