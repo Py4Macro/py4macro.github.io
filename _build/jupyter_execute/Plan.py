4f7d6c1461cd98b3a2b75993e438505ebb7d5005
Tetsu HARUYAMA <tetsu@T-MBP3.local>
2023-02-21 16:02:53 +0900

    students comment

--- _build/jupyter_execute/Plan.py
+++ _build/jupyter_execute/Plan.py
@@ -7,12 +7,22 @@
 # <input type="button" onclick="location.href='https://translate.google.com/translate?hl=&sl=ja&tl=en&u='+window.location;" value="Google translation" style="color:#ffffff;background-color:#008080; height:25px" onmouseover="this.style.background='#99ccff'" onmouseout="this.style.background='#008080'"/> in English or the language of your choice.
 # </div><br>
 
-# 第３部の「マクロ経済学 - 短期分析」は未完成だが，次の目次に沿って進める計画である。2022度後期の「プログラミングと経済分析」の教科書として本サイトの内容を使う予定（もう一つの教科書は[「経済学のためのPython入門」](https://py4basics.github.io)）。
-# * GDPと構成要素 ☑️
-# * 法則・曲線・増加率 ☑️
-# * IS-MP-PCモデル ☑️
-# * ランダム変数と全要素生産性 ☑️
-# * linearsolve ☑️
-# * 実物的景気循環(RBC)モデル ☑️
-# * 貨幣の中立性と金融政策 ☑️
-# * ニューケインジアン・モデル（DSGEモデル）
+# 「短期分析II」は未完成だが，次の目次に沿ってサイトの拡充を進める計画である。
+# * 長期分析
+#     * 発展会計 ☑️
+#     * 成長会計 ☑️
+#     * 回帰分析 ☑️
+#     * 差分方程式と経済分析 ☑️
+#     * ソロー・モデル ☑️
+#     * 所得分布と所得収斂 ☑️
+# * 短期分析I
+#     * GDPと構成要素 ☑️
+#     * 法則・曲線・増加率 ☑️
+#     * 景気循環の定量分析：ADASモデル ☑️
+#     * IS-MP-PCモデル ☑️
+# * 短期分析II
+#     * ランダム変数と全要素生産性 ☑️
+#     * linearsolve ☑️
+#     * 実物的景気循環(RBC)モデル ☑️
+#     * 貨幣の中立性と金融政策 ☑️
+#     * ニューケインジアン・モデル（DSGEモデル）
